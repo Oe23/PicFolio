45a31257d6b5c937d1637bc7009a0a9d945468d5
Oski <Apple@Oskis-MacBook-Pro.local>
2012-09-20 14:59:22 -0500

    Initial Commit

---
+++ Pic-folio/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pic-folio
+//
+//  Created by Oski on 9/20/12.
+//  Copyright (c) 2012 Oscar Edmond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
